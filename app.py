68fb1b5fe5b0a05ecf51e86d99cc4e5aa314b096
juankonker <152643054+juankonker@users.noreply.github.com>
2024-04-16 17:01:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -146,7 +146,6 @@ update_data()
 num_values = 10 # Maximum of data displayed on the graph
 
 df = pd.DataFrame(data_dict)
-data_dict_sorted = pd.DataFrame(data_dict).sort_values(by='Measurement', ascending=True)
 
 subset_df = df.tail(num_values)
 fig = px.line(subset_df, x="Measurement", y="Mass (kTon)", markers=True, template='plotly_dark',
@@ -224,7 +223,7 @@ dash_app.layout = html.Div(
                                 {'name': 'Temperature (°C)', 'id': 'Temperature (°C)'},
                                 
                             ],
-                            data=data_dict_sorted.to_dict('records'),
+                            data=pd.DataFrame(data_dict).to_dict('records'),
                             style_table={'height': 275, 'width': '99%'},
                             style_cell={'textAlign': 'center', 'minWidth': '100px', 'font_size': '18px',
                                         'font_family': 'Arial, sans-serif'},
